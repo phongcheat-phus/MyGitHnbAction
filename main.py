5247635bf7ed792655e6cc5631365ad6340ae002
phongcheat-phus <168249163+phongcheat-phus@users.noreply.github.com>
2025-02-28 14:55:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def sayhello():
-    print("Hello World")
-
-def sayGoodbye():
+def say_Hello():
+    """Print a greeting message."""
     print("Hello, World!")
+
+def say_Goodbye():
+    """Print a greeting message."""
+    print("Hello, World!")
+
